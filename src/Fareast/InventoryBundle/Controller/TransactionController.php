577489622805354b25bfede491f8db3485620584
Terese <terese@quadrantalpha.com>
2015-04-21 13:20:40 +0800

    fixed submit of beginning inventory and transfer stock

--- src/Fareast/InventoryBundle/Controller/TransactionController.php
+++ src/Fareast/InventoryBundle/Controller/TransactionController.php
@@ -96,9 +96,52 @@ class TransactionController extends BaseController
         return new JsonResponse($json);   
     }
 
+    protected function processBeginEntries($data, $prefix)
+    {        
+        $em = $this->getDoctrine()->getManager();
+
+        // figure out setter
+        if ($prefix == 'from')
+            $setter = 'setCredit';
+        else
+            $setter = 'setDebit';
+
+        // initialize entries
+        $entries = array();
+
+        // check if there's anything to process
+        if (!isset($data[$prefix . '_wh_id']))
+            return $entries;
+
+        // process it
+        foreach ($data[$prefix . '_wh_id'] as $index => $wh_id)
+        {
+            $prod_id = $data[$prefix . '_prod_id'][$index];
+            $qty = $data[$prefix . '_qty'][$index];
+
+            // product
+            $prod = $em->getRepository('CatalystInventoryBundle:Product')->find($prod_id);
+            if ($prod == null)
+                throw new ValidationException('Could not find product.');
+
+            // warehouse
+            $wh = $em->getRepository('CatalystInventoryBundle:Warehouse')->find($wh_id);
+            if ($wh == null)
+                throw new ValidationException('Could not find warehouse.');
+
+            $entry = new Entry();
+            $entry->setWarehouse($wh)
+                ->setProduct($prod)
+                ->$setter($qty);
+
+            $entries[] = $entry;
+        }
+
+        return $entries;
+    }
+
     public function addSubmitAction()
     {
-
         $inv = $this->get('catalyst_inventory');
         $log = $this->get('catalyst_log');
 
@@ -106,17 +149,21 @@ class TransactionController extends BaseController
 
         try
         {
-
             $data = $this->getRequest()->request->all();
 
-            $entries = $this->processBeginEntries($data);
+            // process entries
+            $from_ents = $this->processTransferEntries($data, 'from');
+            $to_ents = $this->processTransferEntries($data, 'toform');
+
             // setup transaction
             $trans = new Transaction();
             $trans->setUserCreate($this->getUser())
                 ->setDescription("Adding Items");
 
             // add entries
-            foreach ($entries as $ent)
+            foreach ($from_ents as $ent)
+                $trans->addEntry($ent);
+            foreach ($to_ents as $ent)
                 $trans->addEntry($ent);
 
             $inv->persistTransaction($trans);
@@ -129,57 +176,17 @@ class TransactionController extends BaseController
         catch (ValidationException $e)
         {
             $this->addFlash('error', $e->getMessage());
+
+            return $this->redirect($this->generateUrl('cat_inv_trans_index'));
         }
         catch (InventoryException $e)
         {
             $this->addFlash('error', $e->getMessage());
+
+            return $this->redirect($this->generateUrl('cat_inv_trans_index'));
         }
 
         $this->addFlash('success', 'Transfer transaction successful.');
         return $this->redirect($this->generateUrl('feac_inv_begin_index'));
-    }
-
-    protected function processBeginEntries($data)
-    {        
-        $em = $this->getDoctrine()->getManager();        
-        $inv = $this->get('catalyst_inventory');
-        $conf = $this->get('catalyst_configuration');        
-
-        $supp = $data['wh_opts'];
-
-        $supplier = $em->getRepository('CatalystInventoryBundle:Warehouse')->find($supp);
-            if ($supplier == null)
-                throw new ValidationException('Could not find warehouse.'); 
-
-        //     echo "<pre>";
-        // print_r($data);
-        // print_r($supplier);
-        // // print_r($date);
-        // echo "</pre>";
-        // die();
-        
-        // initialize entries
-        $entries = array();
-
-        // check if there's anything to process
-        if (!isset($data['prod_id']))
-            return $entries;
-
-        // process it
-        foreach ($data['prod_id'] as $index => $prod_id)
-        {
-            $qty = $data['qty'][$index];
-
-            
-            // product
-            $product = $em->getRepository('CatalystInventoryBundle:Product')->find($prod_id);
-            if ($product == null)
-                throw new ValidationException('Could not find product.'); 
-
-                                 
-                        
-            $entries = array_merge($entries,$inv->itemsIn($product,$qty,$supplier));
-        }
-        return $entries;
     }
 }
--- src/Fareast/InventoryBundle/Controller/TransferStockController.php
+++ src/Fareast/InventoryBundle/Controller/TransferStockController.php
@@ -3,6 +3,7 @@
 namespace Fareast\InventoryBundle\Controller;
 
 use Catalyst\TemplateBundle\Model\CrudController;
+use Catalyst\InventoryBundle\Entity\Transaction;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -25,7 +26,7 @@ class TransferStockController extends CrudController
 
     protected function getObjectLabel($obj)
     {
-        
+
     }
 
     public function indexAction()
@@ -36,6 +37,7 @@ class TransferStockController extends CrudController
 
         $params = $this->getViewParams('List');
 
+        // $twig_file = 'CatalystInventoryBundle:Transaction:begin.html.twig';
         $twig_file = 'FareastInventoryBundle:TransferStock:index.html.twig';
 
         $inv = $this->get('catalyst_inventory');
@@ -44,6 +46,7 @@ class TransferStockController extends CrudController
         $params['grid_cols'] = $gl->getColumns();
         $params['wh_opts'] = $inv->getWarehouseOptions();
         $params['prodgroup_opts'] = $inv->getProductGroupOptions();  
+        // $params['prod_opts'] = $inv->getProductGroupOptions();  
 
         return $this->render($twig_file, $params);
     }
@@ -89,14 +92,109 @@ class TransferStockController extends CrudController
             }
         }
 
+        
+
+        return new JsonResponse($data);
+    }
+
+    protected function processTransferEntries($data, $prefix)
+    {
         // echo "<pre>";
-        // print_r($inc_po);
+        // print_r($data);
         // // print_r($ctr);
         // // print_r($date);
         // echo "</pre>";
         // die();
+        $em = $this->getDoctrine()->getManager();
 
-        return new JsonResponse($data);
+        // figure out setter
+        if ($prefix == 'from')
+            $setter = 'setCredit';
+        else
+            $setter = 'setDebit';
+
+        // initialize entries
+        $entries = array();
+
+        // check if there's anything to process
+        if (!isset($data[$prefix . '_wh_id']))
+            return $entries;
+
+        // process it
+        foreach ($data[$prefix . '_wh_id'] as $index => $wh_id)
+        {
+            $prod_id = $data[$prefix . '_prod_id'][$index];
+            $qty = $data[$prefix . '_qty'][$index];
+
+            // product
+            $prod = $em->getRepository('CatalystInventoryBundle:Product')->find($prod_id);
+            if ($prod == null)
+                throw new ValidationException('Could not find product.');
+
+            // warehouse
+            $wh = $em->getRepository('CatalystInventoryBundle:Warehouse')->find($wh_id);
+            if ($wh == null)
+                throw new ValidationException('Could not find warehouse.');
+
+            $entry = new Entry();
+            $entry->setWarehouse($wh)
+                ->setProduct($prod)
+                ->$setter($qty);
+
+            $entries[] = $entry;
+        }
+
+        return $entries;
+    }
+
+    public function addSubmitAction()
+    {
+        $inv = $this->get('catalyst_inventory');
+        $log = $this->get('catalyst_log');
+
+        $em = $this->getDoctrine()->getManager();
+
+        try
+        {
+            $data = $this->getRequest()->request->all();
+
+            // process entries
+            $from_ents = $this->processTransferEntries($data, 'fromform');
+            $to_ents = $this->processTransferEntries($data, 'toform');
+
+            // setup transaction
+            $trans = new Transaction();
+            $trans->setUserCreate($this->getUser())
+                ->setDescription("Transferring Items");
+
+            // add entries
+            foreach ($from_ents as $ent)
+                $trans->addEntry($ent);
+            foreach ($to_ents as $ent)
+                $trans->addEntry($ent);
+
+            $inv->persistTransaction($trans);
+            $em->flush();
+
+            // log
+            $odata = $trans->toData();
+            $log->log('cat_inv_trans_add', 'added Inventory Transaction ' . $trans->getID() . '.', $odata);
+        }
+        catch (ValidationException $e)
+        {
+            $this->addFlash('error', $e->getMessage());
+
+            return $this->redirect($this->generateUrl('cat_inv_trans_index'));
+        }
+        catch (InventoryException $e)
+        {
+            $this->addFlash('error', $e->getMessage());
+
+            return $this->redirect($this->generateUrl('cat_inv_trans_index'));
+        }
+
+        $this->addFlash('success', 'Transfer transaction successful.');
+        return $this->redirect($this->generateUrl('feac_inv_transfer_index'));
     }
 	
 }
