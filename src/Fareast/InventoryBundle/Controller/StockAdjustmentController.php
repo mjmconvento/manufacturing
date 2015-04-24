0c213b52fdefe482ff0c16274f7ff46c32f0bac2
Terese <terese@quadrantalpha.com>
2015-04-24 19:21:26 +0800

    table with editable column in stock adjustment

--- src/Fareast/InventoryBundle/Controller/StockAdjustmentController.php
+++ src/Fareast/InventoryBundle/Controller/StockAdjustmentController.php
@@ -6,9 +6,7 @@ use Catalyst\TemplateBundle\Model\BaseController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Catalyst\InventoryBundle\Entity\Warehouse;
-use Catalyst\GridBundle\Model\FilterGroup;
-use Catalyst\GridBundle\Model\Grid\Column as GCol;
-use Catalyst\GridBundle\Model\Grid\Loader;
+use Catalyst\InventoryBundle\Entity\Transaction;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 
@@ -17,84 +15,140 @@ class StockAdjustmentController extends BaseController
     public function indexAction()
     {
         $this->title = 'Stock Adjustment';
+        $this->route_prefix = 'feac_inv_adjust';
 
         $params = $this->getViewParams('','feac_inv_adjust_index');
 
-        $params['grid_cols'] = $this->getGridColumns();
         $em = $this->getDoctrine()->getManager();
 
         $inv = $this->get('catalyst_inventory');
         $params['dept'] = $inv->getWarehouseOptions();
-        $params['prodgroup_opts'] = $inv->getProductGroupOptions();        
+        $params['prodgroup_opts'] = $inv->getProductGroupOptions();
+
+        $stock = $em->getRepository('CatalystInventoryBundle:Stock')->findAll();
+
+        $data = array();
+        foreach($stock as $s)
+        {
+            if($s->getQuantity() > 0)
+            $data[] = [
+                'code' => $s->getProduct()->getSKU(),
+                'warehouse' => $s->getInventoryAccount()->getName(),
+                'wh_id' => $s->getInventoryAccount()->getID(),
+                'name' => $s->getProduct()->getName(),
+                'prod_id' => $s->getProduct()->getID(),
+                'quantity' =>$s->getQuantity(),
+                'uom' => $s->getProduct()->getUnitOfMeasure(),
+            ];
+        }
+
+        $params['data'] = $data;
 
         return $this->render('FareastInventoryBundle:StockAdjustment:index.html.twig', $params);
     }
 
-    protected function getGridColumns()
+    public function filterAction($dept = null, $category = null)
     {
-        return array(
-            new GCol('Item Code', 'getSKU', 'sku', 'p'),
-            new GCol('Item Name', 'getName', 'name', 'p'),
-            new GCol('Quantity', 'getQuantity', 'qty'),
-            new GCol('Unit of Measure', 'getUnitOfMeasure', 'uom', 'p'),
-        );
+        
     }
 
-    protected function setupGrid()
+    protected function processTransferEntries($data, $prefix)
     {
-        $grid = $this->get('catalyst_grid');
-        $data = $this->getRequest()->query->all();
+        echo "<pre>";
+        print_r($data);
+        // print_r($ctr);
+        // print_r($date);
+        echo "</pre>";
+        die(); 
         $em = $this->getDoctrine()->getManager();
 
-        // setup grid
-        $gl = $grid->newLoader();
-        $gl->processParams($data)
-            ->setRepository('CatalystInventoryBundle:Stock')
-            ->addJoin($grid->newJoin('p', 'product', 'getProduct'))
-            ->addJoin($grid->newJoin('w', 'inv_account', 'getInventoryAccount'))
-            ->enableCountFilter();
-            // ->setQBFilterGroup($fg);
+        // figure out setter
+        if ($prefix == 'from')
+            $setter = 'setCredit';
+        else
+            $setter = 'setDebit';
 
-        // columns
-        $stock_cols = $this->getGridColumns();
-        foreach ($stock_cols as $col)
-            $gl->addColumn($col);
+        // initialize entries
+        $entries = array();
 
-        return $gl;
+        // check if there's anything to process
+        if (!isset($data[$prefix . '_wh_id']))
+            return $entries;
+
+        // process it
+        foreach ($data[$prefix . '_wh_id'] as $index => $wh_id)
+        {
+            $prod_id = $data[$prefix . '_prod_id'][$index];
+            $qty = $data[$prefix . '_qty'][$index];
+
+            // product
+            $prod = $em->getRepository('CatalystInventoryBundle:Product')->find($prod_id);
+            if ($prod == null)
+                throw new ValidationException('Could not find product.');
+
+            // warehouse
+            $wh = $em->getRepository('CatalystInventoryBundle:Warehouse')->find($wh_id);
+            if ($wh == null)
+                throw new ValidationException('Could not find warehouse.');
+
+            $entry = new Entry();
+            $entry->setWarehouse($wh)
+                ->setProduct($prod)
+                ->$setter($qty);
+
+            $entries[] = $entry;
+        }
+
+        return $entries;
     }
 
-    public function gridAction($dept = null, $category = null)
+    public function addSubmitAction()
     {
-        $gl = $this->setupGrid();
-        $qry = array();
-        
-        
-        $grid = $this->get('catalyst_grid');
-        $fg = $grid->newFilterGroup();
+        $inv = $this->get('catalyst_inventory');
+        $log = $this->get('catalyst_log');
 
-        if($dept != null and $dept != 'null' ){
-            $aw_id = explode(',',$dept);
-            $qry[] = 'w.id IN ('. implode(', ', $aw_id).')';
-        }
+        $em = $this->getDoctrine()->getManager();
 
-        if ($category != null and $category != 'null' )
+        try
         {
-            $aw_id = explode(',',$category);
-            $qry[] = 'p.prodgroup IN ('. implode(', ', $aw_id).')';
+            $data = $this->getRequest()->request->all();
+
+            // process entries
+            $from_ents = $this->processTransferEntries($data, 'from');
+            $to_ents = $this->processTransferEntries($data, 'toform');
+
+            // setup transaction
+            $trans = new Transaction();
+            $trans->setUserCreate($this->getUser())
+                ->setDescription("Adding Items");
+
+            // add entries
+            foreach ($from_ents as $ent)
+                $trans->addEntry($ent);
+            foreach ($to_ents as $ent)
+                $trans->addEntry($ent);
+
+            $inv->persistTransaction($trans);
+            $em->flush();
+
+            // log
+            $odata = $trans->toData();
+            $log->log('cat_inv_trans_add', 'added Inventory Transaction ' . $trans->getID() . '.', $odata);
+        }
+        catch (ValidationException $e)
+        {
+            $this->addFlash('error', $e->getMessage());
+
+            return $this->redirect($this->generateUrl('cat_inv_trans_index'));
+        }
+        catch (InventoryException $e)
+        {
+            $this->addFlash('error', $e->getMessage());
+
+            return $this->redirect($this->generateUrl('cat_inv_trans_index'));
         }
 
-        $qry[] = 'o.quantity > 0';
-        
-        if(!empty($qry)){
-            $filter = implode(' AND ', $qry);
-            $fg->where($filter);
-            $gl->setQBFilterGroup($fg);
-        }
-        $gres = $gl->load();
-
-        $resp = new Response($gres->getJSON());
-        $resp->headers->set('Content-Type', 'application/json');
-
-        return $resp;
+        $this->addFlash('success', 'Transfer transaction successful.');
+        return $this->redirect($this->generateUrl('feac_inv_adjust_index'));
     }
 }
